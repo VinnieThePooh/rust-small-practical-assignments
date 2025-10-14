c50d880aade4c11b328a54b0d7ce3f1f03c5826f
Lashkevich Ryan <lashkevichrain@gmail.com>
2025-10-14 20:01:27 +0700

    producer-consumer: variable epochs

--- copy-vs-allocation-bench/src/main.rs
+++ copy-vs-allocation-bench/src/main.rs
@@ -2,12 +2,13 @@ use std::thread;
 use std::time::Instant;
 
 fn main() {
-    array_sharing_bench();
-    vector_sharing_bench();
+    let iter_count = 1_000_000;
+    array_sharing_bench(iter_count);
+    vector_sharing_bench(iter_count);
 }
 
 // new Vec<> instance per iteration
-fn vector_sharing_bench() {
+fn vector_sharing_bench(iter_count: usize) {
 
     println!("Vector sharing bench:");
     let (sender, receiver) = std::sync::mpsc::channel::<Vec<_>>();
@@ -18,11 +19,11 @@ fn vector_sharing_bench() {
         // producer
         s.spawn(move || {
             println!("\tProducer: {:?}", thread::current().id());
-            for _ in 0..10_000 {
+            for _ in 0..iter_count {
 
                 // аллоцируем новый вектор на каждой итерации,
                 // let vec: Vec<_> = rand::random_iter::<u8>().take(1024).collect();
-                let vec: Vec<u8> = Vec::with_capacity(1024);
+                let vec: Vec<u8> = Vec::with_capacity(1024 * 8);
                 sender.send(vec).unwrap();
             }
         });
@@ -37,25 +38,29 @@ fn vector_sharing_bench() {
     });
 }
 
-fn array_sharing_bench() {
+fn array_sharing_bench(iter_count: usize) {
 
     println!("Array copy bench:");
 
     // аллоцируем только один раз
-    let buffer: [u8; 1024] = [0; _];
-    let (sender, receiver) = std::sync::mpsc::channel::<[u8; 1024]>();
+    let buffer: [u8; 1024 * 8] = [0; _];
+    let (sender, receiver) = std::sync::mpsc::channel::<[u8; 1024 * 8]>();
 
     thread::scope(|s| {
         let instant = Instant::now();
         // producer
-        s.spawn(move || {
+        s.spawn(|| {
             println!("\tProducer: {:?}", thread::current().id());
-            for _ in 0..10_000 {
+            for _ in 0..iter_count {
                 // rand::fill(&mut buffer);
                 // просто копируем один и тот же буфер
+                // Sync trait must be implemented for implicit borrowing in other thread
                 sender.send(buffer).unwrap();
+                // receiver.recv().unwrap();
             }
         });
+        
+        // sender.send(buffer).unwrap();
 
         // consumer
         println!("\tConsumer: {:?}", thread::current().id());
