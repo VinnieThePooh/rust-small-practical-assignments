e31258500087bf378bd1f60c064aec9a211600ea
Lashkevich Ryan <lashkevichrain@gmail.com>
2025-10-22 20:56:22 +0700

    wip(): rayon first tries

---
+++ rayon-api/src/main.rs
@@ -0,0 +1,32 @@
+use rayon::prelude::*;
+
+fn main() {
+    // enumerate_call_demo();
+    break_iteration_demo();
+}
+
+
+fn enumerate_call_demo() {
+    let spd:Vec<i32> = (0..10).collect();
+    let _ = spd.par_iter().enumerate().skip(4).for_each(|(i ,v) | {
+        if i == 7 {
+            println!("{i}, {v}");
+        }
+    });
+}
+
+
+// no guarantees on what items processing would be cancelled
+// depends on scheduler
+fn break_iteration_demo() {
+    let spd:Vec<i32> = (0..=10).collect();
+    let _ = spd.par_iter().enumerate().skip(4).try_for_each(|(i ,v) | {
+        println!("checking: {i}, {v}");
+        if i == 7 {
+            println!("Found: {i}, {v}");
+            return Err(());
+        }
+
+        Ok(())
+    });
+}
