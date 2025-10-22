767fc06f0c8c8cb583ef777a9cdd6ce6e424fb4d
Lashkevich Ryan <lashkevichrain@gmail.com>
2025-10-23 01:12:52 +0700

    nchore(): part_iter() from slice

--- rayon-api/src/main.rs
+++ rayon-api/src/main.rs
@@ -2,15 +2,27 @@ use rayon::prelude::*;
 
 fn main() {
     // enumerate_call_demo();
-    break_iteration_demo();
+    // break_iteration_demo();
+    enumerate_with_slice_demo();
 }
 
 
 fn enumerate_call_demo() {
     let spd:Vec<i32> = (0..10).collect();
-    let _ = spd.par_iter().enumerate().skip(4).for_each(|(i ,v) | {
+    let _ = spd.par_iter().enumerate().skip(4).for_each(|(i, v) | {
         if i == 7 {
             println!("{i}, {v}");
+        }
+    });
+}
+
+
+fn enumerate_with_slice_demo() {
+    let spd:Vec<i32> = (0..10).collect();
+    let _ = spd[4..].par_iter().enumerate().for_each(|(i, v) | {
+        println!("checking: {i}, {v}");
+        if i == 5 {
+            println!("Found: {i}, {v}");
         }
     });
 }
@@ -20,7 +32,7 @@ fn enumerate_call_demo() {
 // depends on scheduler
 fn break_iteration_demo() {
     let spd:Vec<i32> = (0..=10).collect();
-    let _ = spd.par_iter().enumerate().skip(4).try_for_each(|(i ,v) | {
+    let _ = spd.par_iter().enumerate().skip(4).try_for_each(|(i, v) | {
         println!("checking: {i}, {v}");
         if i == 7 {
             println!("Found: {i}, {v}");
